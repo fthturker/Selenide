487beec58bd561db93ad3e90f716969349475bf6
Fatih Turker <fthturker80@gmail.com>
2023-04-18 13:19:46 +0300

    18.04.2023

--- src/test/java/stepdefinitions/TestCenterStepDefinitions.java
+++ src/test/java/stepdefinitions/TestCenterStepDefinitions.java
@@ -1,32 +1,42 @@
 package stepdefinitions;
 
+import com.codeborne.selenide.WebDriverRunner;
 import io.cucumber.java.en.And;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
+import org.openqa.selenium.WebDriver;
 import pages.TestCenterPage;
+
+import java.time.Duration;
 
 import static com.codeborne.selenide.Condition.checked;
 import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Condition.visible;
 import static com.codeborne.selenide.Selenide.switchTo;
+import static com.codeborne.selenide.WebDriverRunner.url;
 import static com.codeborne.selenide.impl.Html.text;
+import static java.lang.Thread.currentThread;
 import static java.lang.Thread.sleep;
 
 public class TestCenterStepDefinitions {
-    TestCenterPage testCenterPage=new TestCenterPage();
+    TestCenterPage testCenterPage = new TestCenterPage();
+
     @Given("kullanici adini gir")
     public void kullanici_adini_gir() {
         testCenterPage.kullaniciAdi.setValue("techproed");
     }
+
     @Given("kullanici sifresini gir")
     public void kullanici_sifresini_gir() {
         testCenterPage.kullaniciSifresi.setValue("SuperSecretPassword");
     }
+
     @When("submit butonuna tikla")
     public void submit_butonuna_tikla() {
         testCenterPage.submitButton.click();
     }
+
     @Then("giris yapildigini test et")
     public void giris_yapildigini_test_et() {
         //Assert.assertTrue(testCenterPage.girisMesaji.isDisplayed()); // JUnit
@@ -38,22 +48,22 @@ public class TestCenterStepDefinitions {
     @And("{string} secili degilse sec")
     public void seciliDegilseSec(String arg0) {
         // eger text = Checkbox 1 ve Checkbox 1 secili degilse, Checkbox 1 'e tıkla
-        if(text.equals("Checkbox 1") && !testCenterPage.checkbox1.isSelected()){
+        if (text.equals("Checkbox 1") && !testCenterPage.checkbox1.isSelected()) {
             testCenterPage.checkbox1.click();
             //Assert.assertTrue(testCenterPage.checkbox1.isSelected());
             testCenterPage.checkbox1.shouldBe(checked);
         }
-        if (text.equals("Checkbox 2") && !testCenterPage.checkbox2.isSelected()){
+        if (text.equals("Checkbox 2") && !testCenterPage.checkbox2.isSelected()) {
             testCenterPage.checkbox2.shouldNotBe(checked); // secili olmadıgını dogrulamak icin
             testCenterPage.checkbox2.click();
             testCenterPage.checkbox2.shouldBe(checked); // secili oldugunu dogrulamak icin
         }
-        if (text.equals("Red") && !testCenterPage.redbox.isSelected()){
+        if (text.equals("Red") && !testCenterPage.redbox.isSelected()) {
             testCenterPage.redbox.shouldNotBe(checked); // secili olmadigini test et
             testCenterPage.redbox.click(); // sec
             testCenterPage.redbox.shouldBe(checked); // secili oldugunu test et
         }
-        if (text.equals("Football") && !testCenterPage.footballbox.isSelected()){
+        if (text.equals("Football") && !testCenterPage.footballbox.isSelected()) {
             testCenterPage.footballbox.shouldNotBe(checked); // secili olmadigini test et
             testCenterPage.footballbox.click(); // sec
             testCenterPage.footballbox.shouldBe(checked); // secili oldugunu test et
@@ -72,9 +82,10 @@ public class TestCenterStepDefinitions {
         sleep(3000);
 
         //testCenterPage.gun.selectOptionByValue(String.valueOf(gun)); // value = "15"
-        testCenterPage.gun.selectOption(gun-1); // index = 25-1=24 bu 25. gunu secer
+        testCenterPage.gun.selectOption(gun - 1); // index = 25-1=24 bu 25. gunu secer
         sleep(3000);
     }
+
     // alert step definitions
     @And("alert prompt butonuna tiklar")
     public void alertPromptButonunaTiklar() {
@@ -96,11 +107,23 @@ public class TestCenterStepDefinitions {
 
     @And("switch to frame {int}")
     public void switchToFrame(int frame) {
-        switchTo().frame(frame-1); // 0. indeks = 1. iframe
+        switchTo().frame(frame - 1); // 0. indeks = 1. iframe
     }
 
     @And("kullanici back to techproeducation.com linkine tiklar")
-    public void kullaniciBackToTechproeducationComLinkineTiklar() {
+    public void kullaniciBackToTechproeducationComLinkineTiklar() throws InterruptedException {
         testCenterPage.techProLink.click();
+        System.out.println("Techpro Linkine tiklandi ve Yeni pencere acildi");
+        Thread.sleep(3000);
+        // System.out.println("SAYFA URL I: " + WebDriverRunner.url());
+        System.out.println("SAYFA URL I: " + url()); // Driver hala test page de olacak
+    }
+
+    @And("switch to window {int}")
+    public void switchToWindow(int targetWindow) throws InterruptedException {
+        switchTo().window(targetWindow - 1, Duration.ofSeconds(5)); // index. Duration.ofSeconds(5) zorunlu değil
+        System.out.println("Yeni pencereye gecis yapildi");
+        Thread.sleep(3000);
+        System.out.println("YENI SAYFA URL I: " + url()); // yeni sayfa url ni verecektir.
     }
 }
